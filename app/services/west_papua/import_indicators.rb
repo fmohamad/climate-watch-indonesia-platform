c0e904ef7b2287ea18ebd631fa441ebdce1eee21
Candra Wana <wana.candra@gmail.com>
2020-03-26 15:42:17 +0700

    change behavior of indicator service import indonesia-platform to create or update

--- app/services/west_papua/import_indicators.rb
+++ app/services/west_papua/import_indicators.rb
@@ -3,29 +3,23 @@ class WestPapua::ImportIndicators
 
   headers indicators: [:section, :ind_code, :indicator, :unit],
           indicators_id: [:ind_code, :indicator],
-          indicator_values: [:geoid, :ind_code, :source],
-          adaptation_included: [:geoid, :ind_code, :source, :value]
+          indicator_values: [:geoid, :ind_code, :source]
 
-  INDICATORS_FILEPATH = "#{CW_FILES_PREFIX}indicators/wp_indicators.csv".freeze
-  INDICATORS_ID_FILEPATH = "#{CW_FILES_PREFIX}indicators/wp_indicators_id.csv".freeze
+  INDICATORS_FILEPATH = "#{CW_FILES_PREFIX}west_papua_indicators/wp_indicators.csv"
+  INDICATORS_ID_FILEPATH = "#{CW_FILES_PREFIX}west_papua_indicators/wp_indicators_id.csv"
   INDICATOR_VALUE_FILEPATHS = %W(
-    #{CW_FILES_PREFIX}indicators/wp_population.csv
-    #{CW_FILES_PREFIX}indicators/wp_economic.csv
-    #{CW_FILES_PREFIX}indicators/wp_social.csv
-  ).freeze
-  ADAPTATION_INCLUDED_FILEPATH = "#{CW_FILES_PREFIX}indicators/adaptation_included.csv".freeze
-
-  VULNERABILITY_CLASS_INDICATOR = 'Adap_12'.freeze
+    #{CW_FILES_PREFIX}west_papua_indicators/wp_population.csv
+    #{CW_FILES_PREFIX}west_papua_indicators/wp_economic.csv
+    #{CW_FILES_PREFIX}west_papua_indicators/wp_social.csv
+  )
 
   def call
     return unless all_headers_valid?
 
     ActiveRecord::Base.transaction do
-      cleanup
 
       import_indicators
       import_indicators_id
-      import_adaptation_included
       indicator_values_csv_hash.each do |filepath, csv|
         import_indicator_values(csv, filepath)
       end
@@ -44,9 +38,6 @@ class WestPapua::ImportIndicators
     [
       valid_headers?(indicators_csv, INDICATORS_FILEPATH, headers[:indicators]),
       valid_headers?(indicators_id_csv, INDICATORS_ID_FILEPATH, headers[:indicators_id]),
-      valid_headers?(
-        adapt_included_csv, ADAPTATION_INCLUDED_FILEPATH, headers[:adaptation_included]
-      ),
       indicator_values_csv_hash.map do |filepath, csv|
         valid_headers?(csv, filepath, headers[:indicator_values])
       end
@@ -61,10 +52,6 @@ class WestPapua::ImportIndicators
     @indicators_id_csv ||= S3CSVReader.read(INDICATORS_ID_FILEPATH)
   end
 
-  def adapt_included_csv
-    @adapt_included_csv ||= S3CSVReader.read(ADAPTATION_INCLUDED_FILEPATH)
-  end
-
   def indicator_values_csv_hash
     @indicator_values_csv_hash ||= INDICATOR_VALUE_FILEPATHS.reduce({}) do |acc, filepath|
       acc.merge(filepath => S3CSVReader.read(filepath))
@@ -72,13 +59,15 @@ class WestPapua::ImportIndicators
   end
 
   def import_indicators
-    import_each_with_logging(indicators_csv, INDICATORS_FILEPATH) do |row|
-      Indicator.create!(
-        code: row[:ind_code],
-        section: section(row),
-        name: row[:indicator],
-        unit: row[:unit]
-      )
+    I18n.with_locale(:en) do
+      import_each_with_logging(indicators_csv, INDICATORS_FILEPATH) do |row|
+        Indicator.create!(
+          code: row[:ind_code],
+          section: section(row),
+          name: row[:indicator],
+          unit: row[:unit]
+        )
+      end
     end
   end
 
@@ -91,16 +80,31 @@ class WestPapua::ImportIndicators
     end
   end
 
-  def import_adaptation_included
-    import_each_with_logging(adapt_included_csv, ADAPTATION_INCLUDED_FILEPATH) do |row|
-      IndicatorValue.create!(
-        location: Location.find_by(iso_code3: row[:geoid]),
-        indicator: Indicator.find_by(code: row[:ind_code]),
-        source: row[:source],
-        values: [{value: row[:value]&.titleize}]
-      )
-    end
+  def indicator_attributes(row)
+    {
+      code: row[:ind_code],
+      section: section(row),
+      name: row[:indicator],
+      unit: row[:unit]
+    }
   end
+
+  def create_or_update(attributes)
+    iso_code3 = attributes[:iso_code3]
+    location = Location.find_or_initialize_by(iso_code3: iso_code3)
+    location.update_attributes!(attributes)
+  end
+
+  # def import_adaptation_included
+  #   import_each_with_logging(adapt_included_csv, ADAPTATION_INCLUDED_FILEPATH) do |row|
+  #     IndicatorValue.create!(
+  #       location: Location.find_by(iso_code3: row[:geoid]),
+  #       indicator: Indicator.find_by(code: row[:ind_code]),
+  #       source: row[:source],
+  #       values: [{value: row[:value]&.titleize}]
+  #     )
+  #   end
+  # end
 
   def import_indicator_values(csv, filename)
     import_each_with_logging(csv, filename) do |row|
@@ -117,8 +121,8 @@ class WestPapua::ImportIndicators
 
   def section(row)
     section = row[:section]
-    if %w(agriculture forestry energy).include?(section)
-      'province_circumstances'
+    if %w(population economic social).include?(section)
+      'west_papua_circumstances'
     else
       section
     end
@@ -126,14 +130,15 @@ class WestPapua::ImportIndicators
 
   def values(row)
     row.headers.grep(/\d{4}/).map do |year|
-      {
-        year: year.to_s.sub('_', '-'),
-        value: vulnerability_class_indicator?(row) ? row[year] : row[year]&.delete('%,', ',')&.to_f
-      }
+      {year: year.to_s.sub('_', '-'), value: row[year]&.delete('%,', ',')&.to_f}
+      # {
+      #   year: year.to_s.sub('_', '-'),
+      #   value: vulnerability_class_indicator?(row) ? row[year] : row[year]&.delete('%,', ',')&.to_f
+      # }
     end
   end
 
-  def vulnerability_class_indicator?(row)
-    row[:ind_code] == VULNERABILITY_CLASS_INDICATOR
-  end
+  # def vulnerability_class_indicator?(row)
+  #   row[:ind_code] == VULNERABILITY_CLASS_INDICATOR
+  # end
 end
