8b0e6afda3ca103dadcf1b4db6254a8793e1e871
Candra Wana <wana.candra@gmail.com>
2020-04-08 10:33:07 +0700

    fix typo on west papua locations members

--- app/admin/indonesia_platform/west_papua_locations_members.rb
+++ app/admin/indonesia_platform/west_papua_locations_members.rb
@@ -29,7 +29,7 @@ ActiveAdmin.register_page 'Indonesia Platform West Papua Locations Members' do
     end
 
     def import_worker
-      DataUploader::BaseImportWorker.perform_async(section.id, 'WestPapua::ImportLocationMembers', current_admin_user.email)
+      DataUploader::BaseImportWorker.perform_async(section.id, 'WestPapua::ImportLocationsMembers', current_admin_user.email)
     end
 
     def section_repository
