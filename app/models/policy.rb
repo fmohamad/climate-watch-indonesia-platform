df53b132f412fcfaf7952af08079681163ad97e2
Candra Wana <wana.candra@gmail.com>
2020-05-07 18:08:12 +0700

    remove translates name method on policy, tracing

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -1,7 +1,7 @@
 class Policy < ApplicationRecord
   include Translate
 
-  translates :name
+  # translates :name
 
   validates_presence_of :name, :code, :section, :unit
   validates :code, uniqueness: true
