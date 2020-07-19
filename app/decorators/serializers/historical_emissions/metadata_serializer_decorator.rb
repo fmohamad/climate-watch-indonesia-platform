7bb3728682932d3acaa3d2d27b0693db9039e33e
anggia ramadhan <anggia.ramadhan@alodokter.com>
2020-07-19 19:52:05 +0700

    add record serializer

--- app/decorators/serializers/historical_emissions/metadata_serializer_decorator.rb
+++ app/decorators/serializers/historical_emissions/metadata_serializer_decorator.rb
@@ -1,5 +1,7 @@
 HistoricalEmissions::MetadataSerializer.class_eval do
   attribute :metric
+  attribute :category
+  attribute :sub_category
 
   def sector
     object.sectors.map do |s|
@@ -9,13 +11,25 @@ HistoricalEmissions::MetadataSerializer.class_eval do
 
   def metric
     object.metrics.map do |m|
-      m.slice(:id, :name, :unit, :code)
+      m.slice(:id, :name, :code)
+    end
+  end
+
+  def category
+    object.categories.map do |m|
+      m.slice(:id, :name, :code)
+    end
+  end
+
+  def sub_category
+    object.sub_categories.map do |m|
+      m.slice(:id, :name, :code)
     end
   end
 
   def gas
     object.gases.map do |m|
-      m.slice(:id, :name, :code)
+      m.slice(:id, :name, :unit, :code)
     end
   end
 end
--- app/decorators/serializers/historical_emissions/record_serializer_decorator.rb
+++ app/decorators/serializers/historical_emissions/record_serializer_decorator.rb
@@ -1,8 +1,18 @@
 HistoricalEmissions::RecordSerializer.class_eval do
   belongs_to :metric
+  belongs_to :category
+  belongs_to :sub_category
 
   def metric
     object.metric.code
+  end
+
+  def category
+    object.category.code
+  end
+
+  def sub_category
+    object.sub_category.code
   end
 
   def sector
