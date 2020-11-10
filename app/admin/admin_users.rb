6deb6db70c3cf55b5af08c2c683e2303894d87f1
Candra Wana <wana.candra@gmail.com>
2020-11-10 15:08:18 +0700

    add admin_national on view controller

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -40,7 +40,7 @@ ActiveAdmin.register AdminUser do
   form do |f|
     f.inputs do
       f.input :email
-      f.input :role, as: :select, collection: %w(superuser admin admin_wp),
+      f.input :role, as: :select, collection: %w(superuser admin admin_wp admin_national),
                      selected: 'admin', include_blank: false
       f.input :password
       f.input :password_confirmation
